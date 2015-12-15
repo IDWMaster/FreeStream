0015a96fd78f5499a6711c614d717392b80ea899
Brian Bosak <webadm@elcnet.servehttp.com>
2015-12-14 21:24:37 -0600

    Fixed

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ var startSystem = function(key) {
 
 
 db.onDbReady(function(){
-    console.log('Database engine ready');
+    console.error('Database engine ready');
     EncryptionKeys = db.EncryptionKeys;
     EncryptionKeys.getDefaultKey(function(key){
         console.error('Got default key.');
